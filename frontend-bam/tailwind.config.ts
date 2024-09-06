77644fbfd9167638533f6fe62003838599a9e2ce
Amoshades <netnapalimjoi@gmail.com>
2024-09-06 12:26:12 +0700

    Navbar and search menu

---
+++ frontend-bam/tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        'main-color': '#004C85',
+        'button': '#F8E47D',
+        'fav-btn': '#FF2A2A',
+        'stroke': '#AEABAB',
+        'btn-bg': '#F6F6F6',
+        'details-search': '#555555',
+      },
+      backgroundImage: {
+        'living-bg': "url('/png/living_room.png')",
+        
+      }
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+};
+export default config;
