fd317d1d59090e2f1720f9bad1ad42b625eb735a
Netnapa Limjoi <89064987+Amoshades@users.noreply.github.com>
2024-12-02 00:23:51 +0700

    Create Footer

---
+++ frontend-bam/src/app/component/menu/footer.tsx
@@ -0,0 +1,12 @@
+'use client'
+import Image from "next/image";
+
+export default function Footer() {
+  return (
+    <div className="w-full h-[190px] bg-main-color flex flex-row px-[10%] font-line-bold inset-x-0 bottom-0">
+        <div className="w-[80%] mx-auto py-[70px]">
+            ©2024 สงวนสิทธิ์ โปรเจควิจัยระบบ Recommendation System for Bam
+        </div>
+    </div>
+  );
+}
